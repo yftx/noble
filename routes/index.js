826f8081a5c29c9b1024b7fc5a9dc04c2fa3848d
huangjiang <350064407@qq.com>
2014-07-20 15:15:31 +0800

    添加对ssion的控制

--- routes/index.js
+++ routes/index.js
@@ -362,8 +362,6 @@ module.exports = function(app) {
     });
 
     function checkLogin(req, res, next) { 
-        next();
-        return;
         if (!req.session.user) {
             req.flash('error', '未登入');
             return res.redirect('/login');
